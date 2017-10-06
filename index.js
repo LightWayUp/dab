c9d05c0ad634cc7fe7086da64d2115607ffafdee
VanishedPotato <32491163+VanishedPotato@users.noreply.github.com>
2017-10-06 15:53:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -95,6 +95,9 @@ bot.on("message", function(message) {
         case "invite":
             message.channel.sendMessage("https://discordapp.com/oauth2/authorize?client_id=364399994242859008&scope=bot&permissions=20040010000");
             break;
+        case "pingz":
+            message.channel.send("Pong!" + "`" + bot.ping + "ms" + "`" + ":ping_pong:" );
+            break;
         default:
             message.channel.sendMessage("I do not recognize that. Use <o/!info to get the list of commands.");
     }

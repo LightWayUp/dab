9ee343ec14775abf66625bbac367404f03d116cc
VanishedPotato <32491163+VanishedPotato@users.noreply.github.com>
2017-10-12 15:01:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new dbl({
    id: "364399994242859008"
 })
 
-client.postStats(5, (err, res) => {
+client.postStats(6, (err, res) => {
     if(err) {
         console.error(err)
     } else {

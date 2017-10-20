8d1752877ac1983e1e56c8530d071580e7f90014
VanishedPotato <32491163+VanishedPotato@users.noreply.github.com>
2017-10-20 15:29:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -79,11 +79,13 @@ var bot = new Discord.Client();
 
 bot.on("ready", function() {
     console.log("Ready");
+    
+bot.user.setGame("<!info | Donate: paypal.me/VanishedP")
+
+});
 
 bot.on("ready", function() {
     console.log("Server Count:" + bot.guilds.size);
-    
-bot.user.setGame("<!info | Donate: paypal.me/VanishedP")
 
 });
 

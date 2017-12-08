f15b39bafa7c72f3c748642511c40df848ef76ef
vanishalways <32491163+VanishedPotato@users.noreply.github.com>
2017-12-08 18:52:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const YTDL = require("ytdl-core")
 var fs = require('fs');
 var data = fs.readFileSync('test.json');
 var words = JSON.parse(data);
-
-const dbl = require(`discord-bot-list`)
  
 const client = new dbl({
     token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM2NDM5OTk5NDI0Mjg1OTAwOCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTA3NzI4NzA1fQ.3zFSPSkjAAo550TMIVp_CJ-1YcgOi-Rgcj6NI3gy16",

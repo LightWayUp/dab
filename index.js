a0e11fde6f8fe505aa438bd2c31165008e482ff6
vanishalways <32491163+VanishedPotato@users.noreply.github.com>
2017-12-24 20:15:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function play(connection, message) {
     });
 }
 
-//const PREFIX = "<!", "<@364399994242859008>".some
+const PREFIX = "<!";
 
 var fortunes = [
     "Yes",
@@ -233,7 +233,7 @@ bot.on("message", function(message) {
     let defined = ``;
         defined = message.author.id;
 
-    var args = message.content.substring(("<!", "<@364399994242859008>").some.length).split(" ");
+    var args = message.content.substring(PREFIX.length).split(" ");
 
     switch (args[0].toLowerCase())  {
         //Music
